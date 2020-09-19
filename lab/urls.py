8aacc0fbfcca68f0ea298c40f098c35e8a7d02c6
Atilano (Tilo) Garcia <tilogarcia1@gmail.com>
2020-09-18 22:26:14 -0500

    Added basic backend call

--- lab/urls.py
+++ lab/urls.py
@@ -6,4 +6,6 @@ app_name = 'educate'
 
 urlpatterns = [
     path('', views.home_page, name='home_page'),
+    path(r'backend_call/', views.backend_call, name='backend_call'),
+    path(r'backend_call_func/', views.backend_request_function, name='backend_request_function'),
 ]
--- lab/views.py
+++ lab/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from urllib.parse import urlencode
+
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 
 def home_page(request):
@@ -7,3 +10,24 @@ def home_page(request):
         'content': 'This is the homepage for the Labs Page'
     }
     return render(request, 'lab/home_page.html', context)
+
+
+def backend_call(request):
+    context = {
+        'title': 'Backend Call',
+        'result': request.GET.get('result', None)
+    }
+    return render(request, 'lab/backend_call.html', context)
+
+
+def backend_request_function(request):
+    if request.POST:
+        num1 = int(request.POST.get('number-input'))
+        num2 = int(request.POST.get('number-input1'))
+        product = num1 * num2
+
+        base_url = reverse('lab:backend_call')
+        query_string = urlencode({'result': product})
+        url = '{}?{}'.format(base_url, query_string)
+        return redirect(url)
+    return redirect(reverse('lab:backend_call'))
